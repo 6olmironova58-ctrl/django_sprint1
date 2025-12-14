878cd30bfa7b50589b7ffede802c16255531e648
Antygin Vladimir <vl.antygin@gmail.com>
2025-12-14 13:39:30 +0300

    first setup

---
+++ blogicum/pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+import views
+
+app_name = 'pages'
+
+urlpatterns = [
+    path("about/", views.about, name="about"),
+    path("rules/", views.rules, name="rules"),
+]
